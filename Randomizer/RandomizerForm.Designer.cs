017c79827e8a6b44289c855781dc79afff1451b5
George Dooley <steve@punak.com>
2019-02-03 21:14:20 -0800

    Initial commit with source

---
+++ Randomizer/RandomizerForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Randomizer
+{
+	partial class Randomizer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Randomizer));
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.btnGrabIt = new System.Windows.Forms.Button();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.textCasino = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textMax = new System.Windows.Forms.TextBox();
+			this.textMin = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.IsSplitterFixed = true;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.btnGrabIt);
+			this.splitContainer1.Panel2.Controls.Add(this.btnGo);
+			this.splitContainer1.Panel2.Controls.Add(this.textCasino);
+			this.splitContainer1.Panel2.Controls.Add(this.label2);
+			this.splitContainer1.Panel2.Controls.Add(this.label1);
+			this.splitContainer1.Panel2.Controls.Add(this.textMax);
+			this.splitContainer1.Panel2.Controls.Add(this.textMin);
+			this.splitContainer1.Size = new System.Drawing.Size(372, 669);
+			this.splitContainer1.SplitterDistance = 196;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// btnGrabIt
+			// 
+			this.btnGrabIt.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnGrabIt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnGrabIt.Location = new System.Drawing.Point(3, 444);
+			this.btnGrabIt.Name = "btnGrabIt";
+			this.btnGrabIt.Size = new System.Drawing.Size(166, 54);
+			this.btnGrabIt.TabIndex = 3;
+			this.btnGrabIt.Text = "Grab &It";
+			this.btnGrabIt.UseVisualStyleBackColor = true;
+			this.btnGrabIt.Click += new System.EventHandler(this.OnGrabItClicked);
+			// 
+			// btnGo
+			// 
+			this.btnGo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnGo.Location = new System.Drawing.Point(3, 132);
+			this.btnGo.Name = "btnGo";
+			this.btnGo.Size = new System.Drawing.Size(166, 54);
+			this.btnGo.TabIndex = 3;
+			this.btnGo.Text = "&Go";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.OnGoClicked);
+			// 
+			// textCasino
+			// 
+			this.textCasino.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.textCasino.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.textCasino.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textCasino.ForeColor = System.Drawing.Color.Red;
+			this.textCasino.Location = new System.Drawing.Point(0, 530);
+			this.textCasino.Name = "textCasino";
+			this.textCasino.Size = new System.Drawing.Size(172, 139);
+			this.textCasino.TabIndex = 2;
+			this.textCasino.Text = "label3";
+			this.textCasino.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(19, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Maximum";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(19, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Minimum";
+			// 
+			// textMax
+			// 
+			this.textMax.Location = new System.Drawing.Point(87, 51);
+			this.textMax.Name = "textMax";
+			this.textMax.Size = new System.Drawing.Size(60, 25);
+			this.textMax.TabIndex = 0;
+			this.textMax.TextChanged += new System.EventHandler(this.OnMaxMinTextChanged);
+			// 
+			// textMin
+			// 
+			this.textMin.Location = new System.Drawing.Point(87, 20);
+			this.textMin.Name = "textMin";
+			this.textMin.Size = new System.Drawing.Size(60, 25);
+			this.textMin.TabIndex = 0;
+			this.textMin.TextChanged += new System.EventHandler(this.OnMaxMinTextChanged);
+			// 
+			// Randomizer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(372, 669);
+			this.Controls.Add(this.splitContainer1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "Randomizer";
+			this.Text = "Casino Randomizer";
+			this.Load += new System.EventHandler(this.OnFormLoad);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textMax;
+		private System.Windows.Forms.TextBox textMin;
+		private System.Windows.Forms.Button btnGo;
+		private System.Windows.Forms.Label textCasino;
+		private System.Windows.Forms.Button btnGrabIt;
+	}
+}
+
---
+++ Randomizer/RandomizerForm.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randomizer
+{
+	public partial class Randomizer : Form
+	{
+		const int CASINO_SPEED = 50;
+		const int BLINK_RATE = 500;
+		const int BLINKS = 1;
+		const int MIN_SPEED = 400;
+		const int BUTTON_HEIGHT = 60;
+
+		static readonly Color BLINK_BACK = Color.Green;
+		static readonly Color BLINK_FORE = Color.White;
+
+		Random _random;
+		List<int> _numbersToGet;
+		Timer _casinoTimer;
+		Timer _blinkTimer;
+		bool _backingOff;
+		int _blinks;
+		StackPanel flowLayout;
+
+		public Randomizer()
+		{
+			InitializeComponent();
+			flowLayout = new StackPanel();
+			flowLayout.FlowDirection = FlowDirection.TopDown;
+			flowLayout.Dock = DockStyle.Fill;
+			flowLayout.AutoScroll = true;
+			splitContainer1.Panel1.Controls.Add(flowLayout);
+		}
+
+		private void OnFormLoad(object sender, EventArgs args)
+		{
+			textMin.Text = 0.ToString();
+			textMax.Text = 9.ToString();
+			btnGrabIt.Enabled = false;
+			btnGo.Enabled = true;
+			textCasino.Text = String.Empty;
+
+			flowLayout.WrapContents = false;
+		}
+
+		private void OnMaxMinTextChanged(object sender, EventArgs e)
+		{
+			int min, max;
+			if( int.TryParse(textMin.Text, out min) && 
+				int.TryParse(textMax.Text, out max) &&
+				max > min)
+			{
+				btnGo.Enabled = true;
+			}
+			else
+			{
+				btnGo.Enabled = false;
+			}
+		}
+
+		private void OnGoClicked(object sender, EventArgs args)
+		{
+			try
+			{
+				int min = int.Parse(textMin.Text);
+				int max = int.Parse(textMax.Text);
+				_numbersToGet = new List<int>();
+				for(int x = min;x <= max;x++)
+				{
+					_numbersToGet.Add(x);
+				}
+
+				btnGo.Enabled = false;
+				btnGrabIt.Enabled = true;
+				textMin.Enabled = false;
+				textMax.Enabled = false;
+				flowLayout.Controls.Clear();
+
+				_backingOff = false;
+
+				_casinoTimer = new Timer();
+				_casinoTimer.Interval = CASINO_SPEED;
+				_random = new Random();
+				_casinoTimer.Tick += OnCasinoTick;
+				_casinoTimer.Enabled = true;
+				_casinoTimer.Start();
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+
+		}
+
+		private void OnCasinoTick(object sender, EventArgs e)
+		{
+			int min, max;
+			if(int.TryParse(textMin.Text, out min) && int.TryParse(textMax.Text, out max))
+			{
+				int got = _random.Next(_numbersToGet.Count);
+				int value = _numbersToGet[got];
+				textCasino.Text = value.ToString();
+			}
+
+			if(_backingOff)
+			{
+				int ms = _random.Next(_casinoTimer.Interval + CASINO_SPEED, _casinoTimer.Interval + (CASINO_SPEED * 2));
+				_casinoTimer.Interval = ms;
+				Debug.WriteLine(ms);
+				if(_casinoTimer.Interval > MIN_SPEED)
+				{
+					_backingOff = false;
+					_casinoTimer.Interval = CASINO_SPEED;
+					int number = AddNumber();
+
+					_numbersToGet.Remove(number);
+
+					textCasino.BackColor = BLINK_BACK;
+					StartBlinking();
+				}
+			}
+		}
+
+		private void OnGrabItClicked(object sender, EventArgs args)
+		{
+			try
+			{
+				btnGrabIt.Enabled = false;
+				_backingOff = true;
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+		}
+
+		private int AddNumber()
+		{
+			int number = 0;
+
+			try
+			{
+				Button label = new Button();
+				label.Text = textCasino.Text;
+				label.AutoSize = false;
+				label.Height = BUTTON_HEIGHT;
+				label.TextAlign = ContentAlignment.MiddleCenter;
+				label.Font = textCasino.Font;
+				label.ForeColor = Color.Green;
+				label.BackColor = Color.White;
+				// label.Dock = DockStyle.Top;
+				flowLayout.Controls.Add(label);
+
+				List<Control> controls = new List<Control>();
+				foreach(Control control in flowLayout.Controls)
+				{
+					controls.Add(control);
+				}
+
+				number = int.Parse(label.Text);
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+
+			return number;
+		}
+
+		void StartBlinking()
+		{
+			_casinoTimer.Enabled = false;
+
+			_blinks = 0;
+			_blinkTimer = new Timer();
+			_blinkTimer.Interval = BLINK_RATE;
+			_blinkTimer.Tick += OnBlinkTick;
+			_blinkTimer.Enabled = true;
+			_blinkTimer.Start();
+		}
+
+		private void OnBlinkTick(object sender, EventArgs e)
+		{
+			if(++_blinks > BLINKS)
+			{
+				_blinkTimer.Stop();
+				textCasino.BackColor = Color.White;
+				if(_numbersToGet.Count == 0)
+				{
+					_casinoTimer.Stop();
+					btnGo.Enabled = true;
+					btnGrabIt.Enabled = false;
+				}
+				else
+				{
+					_casinoTimer.Interval = CASINO_SPEED;
+					_casinoTimer.Enabled = true;
+					btnGrabIt.Enabled = true;
+				}
+			}
+			else
+			{
+				if((_blinks % 2) == 0)
+				{
+					textCasino.BackColor = BLINK_BACK;
+				}
+				else
+				{
+					textCasino.BackColor = BLINK_FORE;
+				}
+			}
+
+
+		}
+	}
+}
